23beacd091404d5712797dcb02d2961c8e87edc8
max-ignat <cadillac.maxim@gmail.com>
2023-03-02 10:41:08 +0200

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,93 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import React, { useState, useEffect } from 'react';
+import { Box } from '../App.styled';
+import Form from './Form';
+import Contacts from './Contacts';
+import Filter from './Filter';
+import Modal from './Modal';
+import { ModalButton } from './Modal/Modal.styled';
+import { Title } from './Form/Form.styled';
+import { useSelector, useDispatch } from 'react-redux'; 
+import { addContact, deleteContact } from 'redux/contacts/contacts-slice';
+import { setFilter } from 'redux/filter/filter-slice';
+import { getAllContacts } from 'redux/contacts/contacts-selectors';
+import { getFilter } from 'redux/filter/filter-selectors';
+
+
+
+const App = () => {
+  const contacts = useSelector(getAllContacts); 
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch(); 
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    if (showModal) {
+      setShowModal(false);
+    } else {
+      setShowModal(true);
+    }
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleAddContact = ({ name, number }) => {
+    const lowerCasedName = name.toLowerCase();
+
+    let added = contacts.find(
+      contact => contact.name.toLowerCase() === lowerCasedName
+    );
+    if (added) {
+      alert(`${name}  is already in contacts`);
+      return;
+    }
+
+    dispatch(addContact({ name, number })); 
+
+    toggleModal();
+  };
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+    console.log('deleted');
+  };
+
+  const changeFilter = ({ target }) => {
+    dispatch(setFilter(target.value));
+    // setFilter(target.value);
+  };
+
+  const filteredContacts = () => {
+  const lowerCasedFilter = filter.toLowerCase();
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(lowerCasedFilter)
   );
 };
+
+
+
+
+  return (
+    <Box>
+      <Title>Phone Book</Title>
+      <ModalButton type="button" onClick={toggleModal}>
+        Add contact
+      </ModalButton>
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <Form submitPropValue={handleAddContact} />
+          <ModalButton type="button" onClick={toggleModal}>
+            minimize
+          </ModalButton>
+        </Modal>
+      )}
+      <Filter value={filter} onChange={changeFilter} />
+      <Contacts
+        contacts={filteredContacts()}
+        onDeleteContact={handleDeleteContact}
+      />
+    </Box>
+  );
+};
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import App from 'components/App';
+import { PersistGate } from 'redux-persist/integration/react';
+// import './index.css';
+import { persistor, store } from './redux/store';
+import { Provider } from 'react-redux'; //!give access to store for all componets
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
