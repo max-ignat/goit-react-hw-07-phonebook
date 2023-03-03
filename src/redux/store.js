a5004f1dafb0860bacd2176c78571e3a966c34d3
max-ignat <cadillac.maxim@gmail.com>
2023-03-03 16:32:54 +0200

    2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,38 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import {
-//   persistStore,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// // import storage from 'redux-persist/lib/storage';
-// import rootReducer from './root-reducer'
-
-// export const store = configureStore({
-//   reducer: rootReducer,
-
-//   middleware(getDefaultMiddleWare) {
-//     return getDefaultMiddleWare({
-//       serializableCheck: {
-//         ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     });
-//   },
-// });
-
-
-// export  const persistor = persistStore(store)
-
-
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+import rootReducer from './root-reducer'
 
-import rootReducer from './root-reducer';
-
-const store = configureStore({
+export const store = configureStore({
   reducer: rootReducer,
+
+  middleware(getDefaultMiddleWare) {
+    return getDefaultMiddleWare({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
 });
 
-export default store;
+
+export  const persistor = persistStore(store)
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+
+// import rootReducer from './root-reducer';
+
+// const store = configureStore({
+//   reducer: rootReducer,
+// });
+
+// export default store;
